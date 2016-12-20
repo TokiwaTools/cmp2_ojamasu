5c50ffa509062cf3116deee871143dafb6940176
TokiwaTools <dn.3402@gmail.com>
2016-12-21 03:31:20 +0900

    Implement Basic 100masukeisan

--- cal.js
+++ cal.js
@@ -1,20 +1,99 @@
 $(document).ready(function(){
-  var table = $('.mainTable');
-  createTable(table);
+  createTable();
+  setHeaders();
+  $('.mainTable input').change(function(e) {
+    var target = e.target;
+    var index = getIndexFromInput(target);
+    if (isEqual(index) ) {
+      $(target).attr('readonly', true);
+    }
+    if ( isClearAtRow(index[1]) ) {
+      console.log(index[1] + ' Row is Complete');
+    }
+    if ( isClearAtColumn(index[0]) ) {
+      console.log(index[0] + ' Column is Complete');
+    }
+  });
 });
 
-function createTable(table) {
+function createTable() {
   var html = '';
   for (var j = 0; j < 12; j++) {
-    html += '<tr class="' + j + '">';
+    html += '<tr>';
     for (var i = 0; i < 12; i++) {
-      html += '<td class="' + i + '"></td>';
+      if (i === 0 && j === 0) {
+        html += '<td>+</td>';
+      } else if (j === 0 && i !== 11) {
+        html += '<td id="row-header-' + i + '" class="row-header"></td>';
+      } else if (i === 0 && j !== 11) {
+        html += '<td id="column-header-' + j + '" class="column-header"></td>';
+      } else if (i === 11 || j === 11) {
+        html += '<td></td>';
+      } else {
+        html += '<td>';
+        html += '<input type="number" size="1" min="0" max="99" class="' + i + '-' + j + '">';
+        html += '</td>';
+      }
     }
     html += '</tr>';
   }
-  table.append(html);
+  $('.mainTable').append(html);
 }
 
-function setHeaders(table) {
+function setHeaders() {
+  $('.mainTable .row-header').each(function() {
+    $(this).text(Math.floor(Math.random()*10));
+  });
+  $('.mainTable .column-header').each(function() {
+    $(this).text(Math.floor(Math.random()*10));
+  });
+}
 
+function getIndexFromInput(target) {
+  var index = [];
+  var className = $(target).attr('class');  //クラス名
+  index.push( className.split('-')[0] );  //列のインデックス
+  index.push( className.split('-')[1] ); //行のインデックス
+  return index;
+}
+
+function isEqual(index) {
+  var sum = $(target).val();  //計算結果
+  var headers = getHeaders(index[0], index[1]);  //ヘッダーの値
+
+  console.log(headers[0] + ' + ' + headers[1] + ' = ' + sum);
+  if (sum == (parseInt(headers[0]) + parseInt(headers[1]))) {
+    console.log('true');
+    return true;
+  }
+  console.log('false');
+  return false;
+}
+
+function getHeaders(i, j) {
+  var headers = [];
+  headers.push( $('#row-header-' + i).text() );
+  headers.push( $('#column-header-' + j).text() );
+  return headers;
+}
+
+//class: column-row
+function isClearAtRow(row) {
+  for (var i = 1; i <= 10; i++) {
+    var className = '.mainTable .' + i + '-' + row;
+    if ( $(className).attr('readonly') !== 'readonly') {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isClearAtColumn(column) {
+  for (var i = 1; i <= 10; i++) {
+    var className = '.mainTable .' + column + '-' + i;
+    if ( $(className).attr('readonly') !== 'readonly') {
+      return false;
+    }
+  }
+  return true;
 }

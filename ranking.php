2275d494eeedaa18c57c09ce73f119f9b2fb449a
Tokiwa <tkw.4402@gmail.com>
2017-01-07 20:40:37 +0900

    add ranking pink style
    bugfix ranking

--- ranking.php
+++ ranking.php
@@ -3,31 +3,41 @@
 <head>
 	<meta charset="utf-8">
 	<title>Result</title>
+	<link rel="shortcut icon" type="image/png" href="images/icon.png">
 	<link rel="stylesheet" type="text/css" href="styles/default.css">
-	<link rel="stylesheet" type="text/css" href="styles/pink.css">
+	<link type="text/css" rel="stylesheet" href="styles/pink.css">
+
 </head>
 <?php
 	$pdo = new PDO("sqlite:userRank.sqlite");
 	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
-	$st = $pdo->query("SELECT * FROM userrank;");
+	//レコード10行分まで表示、scoreで自動ソート
+	$st = $pdo->query("SELECT * FROM userrank order by score desc limit 10;");
 ?>
 <body>
+	<!--<div class="title">
+  		<img src="img/title.png" width="300px">
+  		<img src="img/icon.png" width="40px"><br>
+  	</div>-->
 	<div class="title">
-		<img src="images/title2.png">
+		<img src="images/title2.png" width="350px">
 	</div>
+	<br>
 
-	<h2 class="ranking">ランキング</h2>
-
+  	<div class="ranking"><img src="images/king3.png" width="40px"> Ranking <img src="images/king3.png" width="40px"></div>
 	<?php
 		echo "<table class='rankingTable'>";
 		echo "<tr><th>rank</th><th>name</th><th>score</th><th>time</th>";
+		//1位から順にtable表示
+		$rank = 1;
 		while($result = $st->fetch( PDO::FETCH_ASSOC )){
 			echo "<tr>";
-			echo "<td>{$result['id']}</td>";
+			echo "<td>$rank</td>";
 			echo "<td>{$result['name']}</td>";
 			echo "<td>{$result['score']}</td>";
 			echo "<td>{$result['time']}</td>";
 			echo "<tr>";
+			$rank += 1;
 		}
 		echo "</table>";
 	?>

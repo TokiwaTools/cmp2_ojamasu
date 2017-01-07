8539bc9715461279b82ec2573dfb565695402c53
Tokiwa <tkw.4402@gmail.com>
2017-01-07 18:05:13 +0900

    update scoreboard/timerboard style

--- scripts/ready.js
+++ scripts/ready.js
@@ -15,11 +15,27 @@ $(function($) {
       text: 'GO',
       click: function() {
         $(this).dialog('close');
-        gameStart();
       }
     }],
     open: function(event, ui) {
       $(".ui-dialog-titlebar-close").hide();
+    },
+    close: function(event, ui) {
+      gameStart();
+    }
+  });
+  $('.gameoverDialog').dialog({
+    autoOpen: false,
+    modal: true,
+    width: 500,
+    buttons: [{
+      text: 'ランキングを見る',
+      click: function() {
+        $(this).dialog('close');
+      }
+    }],
+    close: function(event, ui) {
+
     }
   });
 });
